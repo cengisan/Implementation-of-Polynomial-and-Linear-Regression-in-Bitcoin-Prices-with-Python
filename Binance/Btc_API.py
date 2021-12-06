27024f7165ab0d6384b82e71cdcd83500b4a3856
cengisan <cengizhanuzuner41@gmail.com>
2021-12-07 01:00:44 +0300

    Bitcoin prediction graph
    I tried to make graph that will predict Bitcoin's future prices with AI. I used two regression. These are linear and polynomial regression and I combined in a graph all of these.

---
+++ Binance/Btc_API.py
@@ -0,0 +1,60 @@
+# @Cengisan
+from binance.client import Client
+import csv
+from numpy import genfromtxt
+from matplotlib import pyplot as plt
+import numpy as np
+api_key = ("Your API KEY")
+api_secret = ("Your API SECRET")
+
+def all_time_prices():
+    #All Datas of Bitcoin
+    client = Client(api_key, api_secret)
+    candlesticks = client.get_historical_klines("BTCUSDT", Client.KLINE_INTERVAL_1DAY, "1 Jan, 2021", "now")
+    candles = candlesticks
+
+    with open('BTCUSDT.csv', 'w', newline='') as csvfile:
+        candlestick_writer = csv.writer(csvfile, delimiter=',')
+        for candlestick in candles:
+            candlestick_writer.writerow(candlestick)
+        csvfile.close()
+
+    def get_historical_price():
+        my_data = genfromtxt('BTCUSDT.csv', delimiter=',')
+        #Data Elimination
+        btc_data = my_data[:,4]
+        Listformat = btc_data.tolist()
+        with open('BTCUSDT.csv', 'w', newline='') as file:
+            index_writer = csv.writer(file, delimiter=',')
+            for i in Listformat:
+                index_writer.writerow([i])
+            csvfile.close()
+    get_historical_price()
+
+def ai_estimation_graph():
+    j = []
+    i = 0
+    while i < len(genfromtxt('BTCUSDT.csv')):
+        j.append(i)
+        i += 1
+    X = np.array(j).reshape(-1, 1)
+    y = np.array(genfromtxt('BTCUSDT.csv')).reshape(-1, 1)
+    #Fitting Linear Regression to the dataset
+    from sklearn.linear_model import LinearRegression
+    lin = LinearRegression()
+    lin.fit(X, y)
+    # Fitting Polynomial Regression to the dataset
+    from sklearn.preprocessing import PolynomialFeatures
+    poly = PolynomialFeatures(degree=4)
+    X_poly = poly.fit_transform(X)
+    lin2 = LinearRegression()
+    lin2.fit(X_poly, y)
+    # Visualising the Polynomial and Linear Regression results
+    plt.scatter(X, y, color='blue')
+    plt.plot(X, lin2.predict(poly.fit_transform(X)),lin.predict(X), color='red')
+    plt.title('GRAPH')
+    plt.xlabel('Day Numbers of the Year')
+    plt.ylabel('Price')
+    plt.show()
+all_time_prices()
+ai_estimation_graph()
